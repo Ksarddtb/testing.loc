456a3c8dd9ef78fa3bb399a5a9ed1bfe3b837b94
Bakhtiyor <admin@srv.uz>
2022-04-18 22:11:32 +0500

    test start

---
+++ database/migrations/2022_04_18_170010_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('name',200)->unique();
+            $table->string('email',100)->unique();
+            $table->unsignedBigInteger('student_class_id');
+            $table->foreign('student_class_id')->references('id')->on('student_classes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
