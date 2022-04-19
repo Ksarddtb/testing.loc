f625ec518de78560dead2133feaa3e1c67d29d65
Ibatov Bakhtiyor <flastdtb@yandex.ru>
2022-04-19 14:44:15 +0500

    classroom

---
+++ app/Http/Controllers/Api/StudentClassController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\StudentClass as ResourceClass;
+use App\Models\StudentClass;
+use Illuminate\Support\Facades\Validator;
+class StudentClassController extends Controller
+{
+
+    public function index()
+    {
+        return ResourceClass::collection(StudentClass::paginate(25));
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required|unique:student_classes,name|string|max:200',
+        ]);
+        $class=StudentClass::create($request->input());
+        return new ResourceClass($class);
+    }
+    public function show(StudentClass $classroom)
+    {
+        return new ResourceClass($classroom);
+    }
+    public function update(Request $request,StudentClass $classroom)
+    {
+        $rules=[
+            'name'=>'required|string|max:200',
+        ];
+        $validator=Validator::make($request->all(),$rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(),400);
+        }
+        $classroom->update($request->all());
+        return new ResourceClass($classroom);
+    }
+    public function destroy(StudentClass $classroom)
+    {
+        $classroom->delete();
+        return response()->json('deleted',200);
+    }
+}
--- app/Models/StudentClass.php
+++ app/Models/StudentClass.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class StudentClass extends Model
 {
     use HasFactory;
+    protected $fillable=['name'];
     /**
      * Get all of the comments for the StudentClass
      *
